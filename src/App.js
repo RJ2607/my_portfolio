f5c5d0dadb29dd4d17050e864c256aabd791ceb0
RJ2607 <rishijain2607@gmail.com>
2023-09-29 00:04:48 +0530

    portfolio

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import React from 'react'
+import Home from './Routes/home'
+import About from './Routes/about'
+import Project from './Routes/project'
+import Contact from './Routes/contact'
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      portfolio
-    </div>
+    <>
+    <Routes>
+      <Route path='/' element = {<Home/>} />
+      <Route path='/' element = {<Project/>} />
+      <Route path='/' element = {<About/>} />
+      <Route path='/' element = {<Contact/>} />
+    </Routes>
+    </>
   );
 }
 
