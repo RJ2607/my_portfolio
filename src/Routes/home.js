874959834d9395d81ada5e255ad9417309b245ed
RJ2607 <rishijain2607@gmail.com>
2023-09-30 23:12:48 +0530

    backimg

--- src/Routes/home.js
+++ src/Routes/home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../components/navbar'
+import backimg from '../components/backimg'
 
 const home = () => {
     return (
         <div>
             <Navbar />
+            <backimg />
         </div>
     )
 }
---
+++ src/components/backimg.js
@@ -0,0 +1,15 @@
+import "./backimgstyles.css";
+
+import React from 'react'
+
+const backimg = () => {
+    return (
+        <div className="back">
+            <div className="mask">
+                
+            </div>
+        </div>
+    )
+}
+
+export default backimg
