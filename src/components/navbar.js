2c2dd68ec770609745437505f252ea0b180b7c07
RJ2607 <rishijain2607@gmail.com>
2023-09-30 18:49:32 +0530

    navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,30 +1,41 @@
 import { Link } from "react-router-dom"
 import "./navbarstyles.css"
+import { FaBars, FaTimes } from "react-icons/fa"
+import React, { useState } from "react"
 
-import React from 'react'
 
-const navbar = () => {
-  return (
-    <div className="header">
-        <Link to = "/">
-        <h1>Portfolio</h1>
-        </Link>
-        <ul className="nav-menu">
-            <li>
-                <Link to = "/">Home</Link>
-            </li>
-            <li>
-                <Link to = "/project">Project</Link>
-            </li>
-            <li>
-                <Link to = "/about">About</Link>
-            </li>
-            <li>
-                <Link to = "/contact">Contact</Link>
-            </li>
-        </ul>
-    </div>
-  )
+const Navbar = () => {
+
+    const [click, setClick] = useState(false);
+    const handleClick = () => setClick(!click);
+
+
+    return (
+        <div className="header">
+            <Link to="/">
+                <h1>Portfolio</h1>
+            </Link>
+            <ul className={click ? "nav-menu active":"nav-menu"}>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/project">Project</Link>
+                </li>
+                <li>
+                    <Link to="/about">About</Link>
+                </li>
+                <li>
+                    <Link to="/contact">Contact</Link>
+                </li>
+            </ul>
+            <div className="hamburger" onClick={handleClick}>
+                {click ? (
+                    <FaTimes size={20} style={{ color: "#fff" }} />) :
+                    (<FaBars size={20} style={{ color: "#fff" }} />)}
+            </div>
+        </div>
+    )
 }
 
-export default navbar
+export default Navbar
