0b9bb6b5c333aaf9d3d09f3daa6b0e4e32b5629b
RJ2607 <rishijain2607@gmail.com>
2023-10-02 00:36:00 +0530

    project

--- src/Routes/project.js
+++ src/Routes/project.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Footer from '../components/Footer'
 import Navbar from '../components/navbar'
 import Backimg2 from '../components/Backimg2'
+import Wordcard from '../components/Wordcard'
 
 
 const project = () => {
@@ -9,6 +10,7 @@ const project = () => {
         <div>
             <Navbar />
             <Backimg2 heading="PROJECTS" text="Some of my recent works"/>
+            <Wordcard />
             <Footer />
             
         </div>
