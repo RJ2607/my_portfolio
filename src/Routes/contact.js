bb43b3f4fef1691f593963e3be9ff5ebfdc639fd
RJ2607 <rishijain2607@gmail.com>
2023-10-02 23:04:18 +0530

    contact

--- src/Routes/contact.js
+++ src/Routes/contact.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from '../components/navbar'
 import Footer from '../components/Footer'
 import Backimg2 from '../components/Backimg2'
+import Form from '../components/Form'
 
 const contact = () => {
     return (
         <div>
             <Navbar />
             <Backimg2 heading="CONTACT" text="Let's have a chat😁"/>
+            <Form />
             <Footer />
         </div>
     )
